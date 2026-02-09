75d5f8d4f77f996fddd6894475f58f557d8664f3
PetrChen <petr.chen@sunnycanadian.cz>
2026-02-09 09:21:52 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+	
+})
+let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.value = 100
